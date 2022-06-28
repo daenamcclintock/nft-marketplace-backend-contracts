dd8796b9bef4018b52e710b57540c3cecbc355d8
daenamcclintock <daena.mcclintock@gmail.com>
2022-06-28 09:18:37 -0400

    added buy-item script and minor edits to delete-item script

--- scripts/buy-item.js
+++ scripts/buy-item.js
@@ -3,21 +3,23 @@ const { moveBlocks } = require("../utils/move-blocks")
 
 const TOKEN_ID = 0
 
-const deleteItem = async () => {
+const buyItem = async () => {
     const nftMarketplace = await ethers.getContract("NftMarketplace")
     const basicNft = await ethers.getContract("BasicNft")
-    const transaction = await nftMarketplace.deleteListing(basicNft.address, TOKEN_ID)
+    const listing = await nftMarketplace.getListing(basicNft.address, TOKEN_ID)
+    const price = listing.price.toString()
+    const transaction = await nftMarketplace.buyItem(basicNft.address, TOKEN_ID, { value: price })
     await transaction.wait(1)
-    console.log("NFT Deleted !")
+    console.log("NFT Purchased !")
     if (network.config.chainId == "31337") {
         await moveBlocks(2, (sleepAmount = 1000))
     }
 }
 
 
-deleteItem()
+buyItem()
     .then(() => process.exit(0))
     .catch((error) => {
-    console.error(error)
-    process.exit(1)
+        console.error(error)
+        process.exit(1)
     })
