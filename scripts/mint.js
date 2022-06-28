347ff6b1a3f82ffae9e7eca6bfc3d9f9e96e53ba
daenamcclintock <daena.mcclintock@gmail.com>
2022-06-28 16:41:49 -0400

    updates to mint script

--- scripts/mint.js
+++ scripts/mint.js
@@ -6,10 +6,10 @@ const PRICE = ethers.utils.parseEther("0.1")
 const mint = async () => {
     const basicNft = await ethers.getContract("BasicNftTwo")
     console.log("Minting NFT...")
-    const mintTx = await basicNft.mintNft()
-    const mintTxReceipt = await mintTx.wait(1)
+    const mintTransaction = await basicNft.mintNft()
+    const mintTransactionReceipt = await mintTransaction.wait(1)
     console.log(
-        `Minted tokenId ${mintTxReceipt.events[0].args.tokenId.toString()} from contract: ${
+        `Minted tokenId ${mintTransactionReceipt.events[0].args.tokenId.toString()} from contract: ${
             basicNft.address
         }`
     )
